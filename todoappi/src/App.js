82f3c642caf28d8d49a36008962b586d307ffd45
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-03-14 14:45:37 +0200

    connected django and react

--- todoappi/src/App.js
+++ todoappi/src/App.js
@@ -1,19 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+
 import './App.css';
 import TodoLista from './TodoLista.js';
 
 
-function App() {
-  const data = [
-    {"otsikko": "Ykkönen"},
-    {"otsikko": "Kakkonen"},
-    {"otsikko": "Kolmonen"},
-  ];
-  return (
-    <div className="App">
-      <TodoLista iteemit={data} />
+export default class App extends React.Component {
+  state = {
+    iteemit: []
+  }
 
-    </div>
-  );
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:8000/api/tehtavat/`)
+      .then(res => {
+        const iteemit = res.data;
+        this.setState({ iteemit });
+      })
+  }
+
+  render() {
+    const data = this.state.iteemit;
+    return (
+      <div className="App">
+        <TodoLista iteemit={data} />
+      </div>
+    );
+  }
 }
 
-export default App;
+// function App() {
+//   const data = [
+//     {"otsikko": "Ykkönen"},
+//     {"otsikko": "Kakkonen"},
+//     {"otsikko": "Kolmonen"},
+//   ];
+//   return (
+//     <div className="App">
+//       <TodoLista iteemit={data} />
+
+//     </div>
+//   );
+// }
+
+
